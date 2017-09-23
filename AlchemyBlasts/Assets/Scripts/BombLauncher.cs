5ddf0e6ef5b2d67862f78cd4eb45198fff174d4c
Mann2019 <manpreetbawa04@gmail.com>
2017-09-23 13:12:49 +0530

    Initial gameplay added, ground texture generated.

---
+++ AlchemyBlasts/Assets/Scripts/BombLauncher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombLauncher : MonoBehaviour {
+
+    public int force;
+
+    private Rigidbody2D rbd;
+
+	// Use this for initialization
+	void Start () {
+        rbd = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            float vert = 2f;
+            float hor = 2f;
+            Vector2 diagonal = new Vector2(hor,vert);
+            rbd.AddForce(diagonal * force, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ AlchemyBlasts/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+    public Texture2D level_map;
+    public ColorToPrefab[] colorMappings;
+
+    // Use this for initialization
+    void Start()
+    {
+        GenerateLevel();
+    }
+
+    void GenerateLevel()
+    {
+        for (int i = 0; i < level_map.width; i++)
+        {
+            for (int j = 0; j < level_map.height; j++)
+            {
+                GenerateTile(i, j);
+            }
+        }
+    }
+
+    void GenerateTile(int i, int j)
+    {
+        Color pixelcolor = level_map.GetPixel(i, j);
+        if (pixelcolor.a == 0)
+        {
+            //pixel is transparent, ignore it
+            return;
+        }
+
+        foreach (ColorToPrefab colorMapping in colorMappings)
+        {
+            if (colorMapping.color.Equals(pixelcolor))
+            {
+                Vector2 position = new Vector2(i, j);
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+}
